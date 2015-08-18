31572d6724ebea40d3d73471142878f9bfdf6063
Mark Sanford <marktsanford@gmail.com>
2015-08-17 18:10:01 -0700

    finished 'implement a custom control'

---
+++ FoodTracker2/RatingControl.swift
@@ -0,0 +1,72 @@
+//
+//  RatingControl.swift
+//  FoodTracker2
+//
+//  Created by Work on 8/17/15.
+//  Copyright © 2015 Mark Sanford. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Properties
+    
+    var rating = 0 {
+        didSet { setNeedsLayout() }
+    }
+    
+    var ratingButtons = [UIButton]()
+    
+    var spacing = 5
+    var stars = 5
+    
+    // MARK: Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let filledStarImage = UIImage(named: "filledStar")
+        let emptyStarImage = UIImage(named: "emptyStar")
+        
+        for _ in 0..<stars {
+            let button = UIButton()
+            
+            button.setImage(emptyStarImage, forState: .Normal)
+            button.setImage(filledStarImage, forState: .Selected)
+            button.setImage(filledStarImage, forState: [.Highlighted, .Selected])
+            
+            button.adjustsImageWhenHighlighted = false
+            
+            button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
+            button.frame = buttonFrame
+        }
+        updateButtonSelectionStates()
+    }
+    
+    // MARK: Action button
+    
+    func ratingButtonTapped(button: UIButton) {
+        rating = ratingButtons.indexOf(button)! + 1
+        //updateButtonSelectionStates()
+    }
+    
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerate() {
+            button.selected = index < rating
+        }
+    }
+    
+}
+
